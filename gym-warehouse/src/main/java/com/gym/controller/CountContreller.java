8eba5e2d7844ddf1f9c2aebce8905776406fcc6f
yehaozhi <1029640532@qq.com>
2019-10-23 08:46:44 +0800

    11

---
+++ gym-warehouse/src/main/java/com/gym/controller/CountContreller.java
@@ -0,0 +1,14 @@
+package com.gym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/count")
+@Controller
+public class CountContreller {
+
+	@RequestMapping("/leave")
+	public void findleaveById() {
+		
+	}
+}
