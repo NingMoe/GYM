68bd7649a3b2138ad5eab25ff0562ba36574491a
yehaozhi <1029640532@qq.com>
2019-10-23 08:44:35 +0800

    11

--- gym-manage/src/main/java/com/gym/config/SpringShiroConfig.java
+++ gym-manage/src/main/java/com/gym/config/SpringShiroConfig.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
-@Configuration
+//@Configuration
 public class SpringShiroConfig {
 	
 	  @Bean public CacheManager newCacheManager() { return new
